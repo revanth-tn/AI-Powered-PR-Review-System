439f484c8103140c03948a57411ba27bb6503ae9
Revanth TN <revanthtnr72@gmail.com>
2026-06-13 23:43:03 +0000

    added comment_response

--- github_client.py
+++ github_client.py
@@ -17,6 +17,8 @@ def get_diff(repo_name,pr_number):
 def post_comment(repo_name, pr_number, commit_sha, comment_data):
     url = "https://api.github.com/repos/" + repo_name + "/issues//" + str(pr_number) + "/comments"
     response = httpx.post(url,headers=HEADERS, json=comment_data)
+    comment_response = post_comment(repo_name, pr_number, commit_sha, {"body": comment_body})
+    print("comment_response",comment_response)
     return response.json()
 
 def set_merge_status( repo_name, commit_sha, state, description):
